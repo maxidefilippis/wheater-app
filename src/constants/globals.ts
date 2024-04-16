bb9b0d5131dba6644cc85ff213ae56334e73d851
maxidefilippis <maxidefilippis@gmail.com>
2024-04-16 06:41:37 -0300

    config global variables

--- src/constants/globals.ts
+++ src/constants/globals.ts
@@ -1,2 +1,2 @@
-export const apiKey = process.env.API_KEY;
-export const apiUrl = process.env.API_URL;
+export const apiKey = process.env.NEXT_PUBLIC_API_KEY;
+export const apiUrl = process.env.NEXT_PUBLIC_API_URL;
