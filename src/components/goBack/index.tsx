30a32c64071d384526e8b84872e67d4ac4458f70
maxidefilippis <maxidefilippis@gmail.com>
2024-04-17 13:19:01 -0300

    updated goBack component

--- src/components/goBack/index.tsx
+++ src/components/goBack/index.tsx
@@ -1,13 +1,33 @@
 import Link from 'next/link';
 import { Typography } from '../typografhy';
 import styles from './index.module.css';
+import { FullContainer } from '../container';
+import { TextType } from '@/constants/textType';
 
-export const GoBack = () => {
+interface BackComponentProps {
+    message?: string;
+}
+export const BackComponent = ({ message }: BackComponentProps) => {
+    if (message) return <GoBackPage message={message} />;
+
+    return <BackButton />;
+};
+
+export const BackButton = () => {
     return (
         <div className={styles.back}>
             <Link href={'/'}>
                 <Typography text="Volver" />
             </Link>
         </div>
+    );
+};
+
+const GoBackPage = ({ message }: { message: string }) => {
+    return (
+        <FullContainer>
+            <Typography text={message} type={TextType.SUBTITLE} />
+            <BackButton />
+        </FullContainer>
     );
 };
