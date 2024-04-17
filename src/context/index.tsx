1941e703be0726c05902980e9c7dc5695d00721d
maxidefilippis <maxidefilippis@gmail.com>
2024-04-17 00:53:08 -0300

    added favorites to localStorage

--- src/context/index.tsx
+++ src/context/index.tsx
@@ -1,7 +1,8 @@
 'use client';
+import { LocalStorageKey, LocalStorageService } from '@/app/services/localStorageService';
 import { isCityInFavorites } from '@/functions/isCityInFavorites';
 import { WeatherData } from '@/models/weatherData';
-import { createContext, useContext, useState } from 'react';
+import { createContext, useContext, useEffect, useRef, useState } from 'react';
 
 interface Context {
     favorites: WeatherData[];
@@ -15,19 +16,31 @@ const AppContext = createContext<Context | undefined>(undefined);
 export const AppWrapper = ({ children }: { children: React.ReactNode }) => {
     const [loading, setLoading] = useState<boolean>(false);
     const [favorites, setFavorites] = useState<WeatherData[]>([]);
+    const localStorage = new LocalStorageService();
+    const initialRender = useRef(true);
 
     const handleLoading = (isLoading: boolean) => {
         setLoading(isLoading);
     };
-
     const handleFavorite = (newFavorite: WeatherData) => {
-        const isCityInFavs = isCityInFavorites(newFavorite, favorites);
-        if (!isCityInFavs) {
-            setFavorites((prevFavorites) => [...prevFavorites, newFavorite]);
-        } else {
-            setFavorites((prevFavorites) => prevFavorites.filter((favorite) => favorite.id !== newFavorite.id));
-        }
+        const isCityInFavs: boolean = isCityInFavorites(newFavorite, favorites);
+
+        !isCityInFavs && setFavorites((prev) => [...prev, newFavorite]);
+        isCityInFavs && setFavorites((prev) => prev.filter((favorite) => favorite.id !== newFavorite.id));
     };
+
+    useEffect(() => {
+        const favoritesFromLocalStorage: WeatherData[] | null = localStorage.getItem(LocalStorageKey.FAVORITES);
+        favoritesFromLocalStorage && setFavorites(favoritesFromLocalStorage);
+    }, []);
+
+    useEffect(() => {
+        if (initialRender.current) {
+            initialRender.current = false;
+            return;
+        }
+        localStorage.setItem(LocalStorageKey.FAVORITES, favorites);
+    }, [favorites]);
 
     return <AppContext.Provider value={{ favorites, handleFavorite, loading, handleLoading }}>{children}</AppContext.Provider>;
 };
